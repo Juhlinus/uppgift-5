40b21277e43c682093a0594dfc8c57cafd71646c
P1raten <juhlinus@gmail.com>
2016-02-17 12:01:15 +0100

    Initial

---
+++ src/PersonalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Mvc;
+using PersonalWebsite.Models;
+using System.Linq;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Author.ToList());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "My contact information.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/PersonalWebsite/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PersonalWebsite.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
